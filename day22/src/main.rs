5ac6db88b64c7938f6e78eed6203d697397f2078
paubt <pau_b@protonmail.com>
2022-02-09 17:22:19 +0100

    the solution

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,3 +1,228 @@
+#[derive(Debug, Clone, Copy)]
+struct GameState {
+    player_hp: i32,
+    mana: i32,
+    mana_spend: u32,
+    boss_hp: i32,
+    boss_damage: u32,
+    shield_rest_turns: u32,
+    poison_rest_turns: u32,
+    recharge_rest_turns: u32,
+    win: Option<bool>,
+}
+
+impl GameState {
+    fn new(
+        player_hp: i32,
+        mana: i32,
+        mana_spend: u32,
+        boss_hp: i32,
+        boss_damage: u32,
+        shield_rest_turns: u32,
+        poison_rest_turns: u32,
+        recharge_rest_turns: u32,
+        win: Option<bool>,
+    ) -> GameState {
+        GameState {
+            player_hp,
+            mana,
+            mana_spend,
+            boss_hp,
+            boss_damage,
+            shield_rest_turns,
+            poison_rest_turns,
+            recharge_rest_turns,
+            win,
+        }
+    }
+    fn from(&self) -> GameState {
+        GameState {
+            player_hp: self.player_hp,
+            mana: self.mana,
+            mana_spend: self.mana_spend,
+            boss_hp: self.boss_hp,
+            boss_damage: self.boss_damage,
+            shield_rest_turns: self.shield_rest_turns,
+            poison_rest_turns: self.poison_rest_turns,
+            recharge_rest_turns: self.recharge_rest_turns,
+            win: self.win,
+        }
+    }
+}
+
+fn part_one() -> u32 {
+    let start_game_state: GameState = GameState::new(50, 500, 0, 71, 10, 0, 0, 0, None);
+    //let test_game_state: GameState = GameState::new(14, 250, 0, 13, 8, 0, 0, 0, None);
+    let mut open_game_states: Vec<(bool, GameState)> = vec![(true, start_game_state)];
+    let mut finished_game_state: Vec<GameState> = vec![];
+
+    fn expand_gamestate(in_game_state: GameState) -> Option<Vec<(bool, GameState)>> {
+        fn decrement_effects(mut g: GameState) -> (GameState, u32) {
+            let temporary_armor: u32;
+            {
+                if g.shield_rest_turns > 0 {
+                    g.shield_rest_turns = g.shield_rest_turns - 1;
+                    temporary_armor = 7;
+                } else {
+                    temporary_armor = 0;
+                }
+                if g.poison_rest_turns > 0 {
+                    g.poison_rest_turns = g.poison_rest_turns - 1;
+                    g.boss_hp = g.boss_hp - 3;
+                }
+                if g.recharge_rest_turns > 0 {
+                    g.recharge_rest_turns = g.recharge_rest_turns - 1;
+                    g.mana = g.mana + 101;
+                }
+            }
+            (g, temporary_armor)
+        }
+
+        let mut new_game_states: Vec<(bool, GameState)> = vec![];
+        // update the effects
+        let (mut in_game_state, _) = decrement_effects(in_game_state);
+        // check if poison killed boss ->  ergo win
+        if in_game_state.boss_hp <= 0 {
+            in_game_state.win = Some(true);
+            return Some(vec![(false, in_game_state)]);
+        }
+        // player turn
+        // go through the spells and add them as possible game state to the return Vec
+        {
+            // magic missle
+            if in_game_state.mana >= 53 {
+                let mut gs: GameState = in_game_state.from();
+                gs.mana = gs.mana - 53;
+                gs.mana_spend = gs.mana_spend + 53;
+                gs.boss_hp = gs.boss_hp - 4;
+                if gs.boss_hp > 0 {
+                    new_game_states.push((true, gs));
+                } else {
+                    gs.win = Some(true);
+                    new_game_states.push((false, gs));
+                }
+            }
+            // drain
+            if in_game_state.mana >= 73 {
+                let mut gs: GameState = in_game_state.from();
+                gs.mana = gs.mana - 73;
+                gs.mana_spend = gs.mana_spend + 73;
+                gs.boss_hp = gs.boss_hp - 2;
+                gs.player_hp = gs.player_hp + 2;
+                if gs.boss_hp > 0 {
+                    new_game_states.push((true, gs));
+                } else {
+                    gs.win = Some(true);
+                    new_game_states.push((false, gs));
+                }
+            }
+            // shield
+            if in_game_state.mana >= 113 && in_game_state.shield_rest_turns == 0 {
+                let mut gs: GameState = in_game_state.from();
+                gs.mana = gs.mana - 113;
+                gs.mana_spend = gs.mana_spend + 113;
+                gs.shield_rest_turns = 6;
+                new_game_states.push((true, gs));
+            }
+            // poison
+            if in_game_state.mana >= 173 && in_game_state.poison_rest_turns == 0 {
+                let mut gs: GameState = in_game_state.from();
+                gs.mana = gs.mana - 173;
+                gs.mana_spend = gs.mana_spend + 173;
+                gs.poison_rest_turns = 6;
+                new_game_states.push((true, gs));
+            }
+            // recharge
+            if in_game_state.mana >= 229 && in_game_state.recharge_rest_turns == 0 {
+                let mut gs: GameState = in_game_state.from();
+                gs.mana = gs.mana - 229;
+                gs.mana_spend = gs.mana_spend + 229;
+                gs.recharge_rest_turns = 5;
+                new_game_states.push((true, gs));
+            }
+        }
+
+        // boss turn for all that have true for still not decided
+        let new_game_states: Vec<(bool, GameState)> = new_game_states
+            .iter_mut()
+            .map(|(b, g)| {
+                if *b == true {
+                    // decrement effects
+                    let (mut g, t) = decrement_effects(*g);
+                    let mut actual_boss_damage = g.boss_damage - t;
+                    if actual_boss_damage <= 0 {
+                        actual_boss_damage = 1;
+                    }
+                    g.player_hp = g.player_hp - actual_boss_damage as i32;
+                    return if g.player_hp <= 0 {
+                        g.win = Some(false);
+                        (false, g)
+                    } else {
+                        (true, g)
+                    };
+                } else {
+                    g.win = Some(false);
+                    return (false, *g);
+                };
+            })
+            .collect();
+        // if the vec is empty this means that not enugth mana ergo RIP
+        if new_game_states.len() == 0 {
+            in_game_state.win = Some(false);
+
+            Some(vec![(false, in_game_state)])
+        } else {
+            Some(new_game_states)
+        }
+    }
+
+    while !open_game_states.is_empty() {
+        // expand each game
+        let new_game_states: Vec<(bool, GameState)> = open_game_states
+            .iter()
+            .map(|(_, g)| expand_gamestate(*g).unwrap())
+            .fold(vec![], |mut acc, mut x| {
+                acc.append(&mut x);
+                acc
+            });
+        // move all finished ones to finished Vec
+        let mut f_g: Vec<GameState> = new_game_states
+            .iter()
+            .filter(|(b, g)| *b == false)
+            .map(|(_, g)| *g)
+            .collect();
+        finished_game_state.append(&mut f_g);
+        // keep all the ones with true (ergo that need further expanding)
+        open_game_states = new_game_states
+            .iter()
+            .filter(|(b, g)| *b == true)
+            .map(|(_, g)| (true, *g))
+            .collect();
+        //print!("{:#?}\n###########\n\n", finished_game_state);
+    }
+    // go through the finished ones and find the
+
+    let t: GameState = finished_game_state
+        .into_iter()
+        .filter(|g| g.win.unwrap() == true)
+        .min_by_key(|g| g.mana_spend)
+        .unwrap();
+    print!("{:#?}", t);
+
+    0
+}
+
+fn part_two() -> u32 {
+    0
+}
+
 fn main() {
-    println!("Hello, world!");
+    let answer_part_one = part_one();
+
+    let answer_part_two = part_two();
+
+    println!(
+        "answer part 1: {}\nanswer part 2: {}",
+        answer_part_one, answer_part_two
+    );
 }
--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,3 +1,219 @@
+use std::fs;
+
+#[derive(Debug)]
+enum InstructionSet {
+    Hlf(char),
+    Tpl(char),
+    Inc(char),
+    Jmp(i32),
+    Jie(char, i32),
+    Jio(char, i32),
+}
+
+fn part_one(instructions: &str) -> u32 {
+    use InstructionSet::*;
+    let instructions: Vec<InstructionSet> = instructions
+        .lines()
+        .map(|l| {
+            // not first tow chars not "ji" -> ergo unary operation
+            let s = l.split(" ").collect::<Vec<&str>>();
+            return match s[0] {
+                "hlf" => Hlf(s[1].chars().next().unwrap()),
+                "tpl" => Tpl(s[1].chars().next().unwrap()),
+                "inc" => Inc(s[1].chars().next().unwrap()),
+                "jmp" => Jmp(s[1].parse::<i32>().unwrap()),
+                "jie" => Jie(s[1].chars().next().unwrap(), s[2].parse::<i32>().unwrap()),
+                "jio" => Jio(s[1].chars().next().unwrap(), s[2].parse::<i32>().unwrap()),
+                _ => panic!("no match for instruction"),
+            };
+        })
+        .collect();
+
+    // let mut instrucion_counter: u32 = 0;
+    let mut reg_a: u32 = 0;
+    let mut reg_b: u32 = 0;
+    let mut exec_position: i32 = 0;
+    //println!("{:?}", instructions);
+
+    'outta: while 0 <= exec_position && exec_position < instructions.len() as i32 {
+        /*
+        {
+            println!(
+                "counter:{} rega|b={}|{} exec_pos={} {:?}",
+                instrucion_counter,
+                reg_a,
+                reg_b,
+                exec_position,
+                instructions[exec_position as usize]
+            );
+            instrucion_counter = instrucion_counter + 1;
+        }
+        */
+        match instructions[exec_position as usize] {
+            Hlf(r) => match r {
+                'a' => reg_a = reg_a / 2,
+                'b' => reg_b = reg_b / 2,
+                _ => panic!("hlf panic"),
+            },
+            Tpl(r) => match r {
+                'a' => reg_a = reg_a * 3,
+                'b' => reg_b = reg_b * 3,
+                _ => panic!("tpl panic"),
+            },
+            Inc(r) => match r {
+                'a' => reg_a = reg_a + 1,
+                'b' => reg_b = reg_b + 1,
+                _ => panic!("inc panic"),
+            },
+            Jmp(d) => {
+                exec_position = exec_position + d;
+                continue 'outta;
+            }
+            Jie(r, d) => match r {
+                'a' => {
+                    if reg_a % 2 == 0 {
+                        exec_position = exec_position + d;
+                        continue 'outta;
+                    }
+                }
+                'b' => {
+                    if reg_b % 2 == 0 {
+                        exec_position = exec_position + d;
+                        continue 'outta;
+                    }
+                }
+                _ => panic!("jie panic"),
+            },
+            Jio(r, d) => match r {
+                'a' => {
+                    if reg_a == 1 {
+                        exec_position = exec_position + d;
+                        continue 'outta;
+                    }
+                }
+                'b' => {
+                    if reg_b == 1 {
+                        exec_position = exec_position + d;
+                        continue 'outta;
+                    }
+                }
+                _ => panic!("jio panic"),
+            },
+        }
+
+        exec_position = exec_position + 1;
+    }
+
+    reg_b
+}
+
+fn part_two(instructions: &str) -> u32 {
+    use InstructionSet::*;
+    let instructions: Vec<InstructionSet> = instructions
+        .lines()
+        .map(|l| {
+            // not first tow chars not "ji" -> ergo unary operation
+            let s = l.split(" ").collect::<Vec<&str>>();
+            return match s[0] {
+                "hlf" => Hlf(s[1].chars().next().unwrap()),
+                "tpl" => Tpl(s[1].chars().next().unwrap()),
+                "inc" => Inc(s[1].chars().next().unwrap()),
+                "jmp" => Jmp(s[1].parse::<i32>().unwrap()),
+                "jie" => Jie(s[1].chars().next().unwrap(), s[2].parse::<i32>().unwrap()),
+                "jio" => Jio(s[1].chars().next().unwrap(), s[2].parse::<i32>().unwrap()),
+                _ => panic!("no match for instruction"),
+            };
+        })
+        .collect();
+
+    // let mut instrucion_counter: u32 = 0;
+    let mut reg_a: u32 = 1;
+    let mut reg_b: u32 = 0;
+    let mut exec_position: i32 = 0;
+    //println!("{:?}", instructions);
+
+    'outta: while 0 <= exec_position && exec_position < instructions.len() as i32 {
+        /*
+        {
+            println!(
+                "counter:{} rega|b={}|{} exec_pos={} {:?}",
+                instrucion_counter,
+                reg_a,
+                reg_b,
+                exec_position,
+                instructions[exec_position as usize]
+            );
+            instrucion_counter = instrucion_counter + 1;
+        }
+        */
+        match instructions[exec_position as usize] {
+            Hlf(r) => match r {
+                'a' => reg_a = reg_a / 2,
+                'b' => reg_b = reg_b / 2,
+                _ => panic!("hlf panic"),
+            },
+            Tpl(r) => match r {
+                'a' => reg_a = reg_a * 3,
+                'b' => reg_b = reg_b * 3,
+                _ => panic!("tpl panic"),
+            },
+            Inc(r) => match r {
+                'a' => reg_a = reg_a + 1,
+                'b' => reg_b = reg_b + 1,
+                _ => panic!("inc panic"),
+            },
+            Jmp(d) => {
+                exec_position = exec_position + d;
+                continue 'outta;
+            }
+            Jie(r, d) => match r {
+                'a' => {
+                    if reg_a % 2 == 0 {
+                        exec_position = exec_position + d;
+                        continue 'outta;
+                    }
+                }
+                'b' => {
+                    if reg_b % 2 == 0 {
+                        exec_position = exec_position + d;
+                        continue 'outta;
+                    }
+                }
+                _ => panic!("jie panic"),
+            },
+            Jio(r, d) => match r {
+                'a' => {
+                    if reg_a == 1 {
+                        exec_position = exec_position + d;
+                        continue 'outta;
+                    }
+                }
+                'b' => {
+                    if reg_b == 1 {
+                        exec_position = exec_position + d;
+                        continue 'outta;
+                    }
+                }
+                _ => panic!("jio panic"),
+            },
+        }
+
+        exec_position = exec_position + 1;
+    }
+
+    reg_b
+}
+
 fn main() {
-    println!("Hello, world!");
+    let instructions: String =
+        fs::read_to_string("../dataSources/day23/input.csv").expect("read in file failed");
+
+    let answer_part_one = part_one(&instructions);
+
+    let answer_part_two = part_two(&instructions);
+
+    println!(
+        "answer part 1: {}\nanswer part 2: {}",
+        answer_part_one, answer_part_two
+    );
 }
